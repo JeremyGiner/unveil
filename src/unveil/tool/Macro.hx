24510d375e4752b64b8937cf36ccc289f0abc6ba
jeremyginer <remy.giner@gmail.com>
2020-06-13 17:15:36 +0200

    Add macro for template building

---
+++ src/unveil/tool/Macro.hx
@@ -0,0 +1,25 @@
+package unveil.tool;
+import haxe.io.Path;
+import haxe.macro.Context;
+import haxe.macro.Expr;
+import haxe.macro.Type;
+import sys.FileSystem;
+import sys.io.File;
+
+class Macro {
+	public static function buildTemplate( s :String  ) :Array<Field> {
+		
+		var aFile = FileSystem.readDirectory( s );
+		for ( sFileName in aFile ) {
+			
+			var sResName = Path.withoutExtension(sFileName);
+			Context.addResource( sResName, File.getBytes(Path.join([s,sFileName])) );
+		}
+		
+		// get the current fields of the class
+		var fields:Array<Field> = Context.getBuildFields();
+		
+		
+		return fields;
+	}
+}
